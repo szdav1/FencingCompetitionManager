112e0152247e11337a3d10418a3665f59df616bf
SzDave <szantodavid.mail@gmail.com>
2024-06-04 23:46:49 +0200

    Update hungarian language and create new color theme

--- src/main/java/com/_4th_dimension_software/app/run/App.java
+++ src/main/java/com/_4th_dimension_software/app/run/App.java
@@ -5,7 +5,6 @@ import com._4th_dimension_software.support.lang.loader.LanguagePackLoader;
 import com._4th_dimension_software.support.settings.Settings;
 import com._4th_dimension_software.support.settings.loader.SettingsLoader;
 import com._4th_dimension_software.support.theme.loader.ColorThemeLoader;
-import com._4th_dimension_software.support.theme.reader.NewColorThemeReader;
 
 /**
  * The <code>App</code> class is the entry point of
@@ -15,23 +14,21 @@ import com._4th_dimension_software.support.theme.reader.NewColorThemeReader;
  * @author szd
  */
 public final class App {
-//	private final XFrame frame;
+	private final XFrame frame;
 
 	/**
 	 * Initializes the application and runs it.
 	 * This is the entry point of the application.
 	 */
 	public App() {
-//		// Read the settings file
-//		SettingsLoader.loadFromXML("/settings/Settings.xml");
-//		// Interpret the specified color theme and open the application window
-//		ColorThemeLoader.loadFromXML(Settings.get("colorTheme"));
-//		// Load the specified language XML file
-//		LanguagePackLoader.loadFromXML(Settings.get("language"));
-//
-//		// Open application window
-//		this.frame = new XFrame("CompetitionManager 0.5-WORK-IN-PROGRESS", "window");
+		// Read the settings file
+		SettingsLoader.loadFromXML("/settings/Settings.xml");
+		// Interpret the specified color theme and open the application window
+		ColorThemeLoader.loadFromXML(Settings.get("colorTheme"));
+		// Load the specified language XML file
+		LanguagePackLoader.loadFromXML(Settings.get("language"));
 
-		NewColorThemeReader.readFromXML("/themes/DarkRedTheme.xml");
+		// Open application window
+		this.frame = new XFrame("CompetitionManager 0.5-WORK-IN-PROGRESS", "window");
 	}
 }
